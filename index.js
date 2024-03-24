72b84da164c44db2b4e5f1a84d9821ad8d580a78
hemat <hematullahw@gmail.com>
2024-03-25 00:41:56 +0430

    watchlist add, color theme bug and remove feture for watchlist

--- index.js
+++ index.js
@@ -7,43 +7,41 @@ let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
 
 
 let foundMovie;
-async function getMovies(movieName = "ninja") {
+async function getMovies(movieName = "impossible") {
     try {
         const res = await fetch(`https://www.omdbapi.com/?apikey=13103cab&s=${movieName}&plot=full`);
         if (!res.ok) {
             throw Error("No movie found!")
         }
-        data = await res.json();
+        let data = await res.json();
         moviesEl.innerHTML = ""
-        data.Search.forEach(item => {
-            fetch(`https://www.omdbapi.com/?apikey=13103cab&i=${item.imdbID}`)
-                .then(res => res.json())
-                .then(movie => {
+        data.Search.forEach(async item => {
+            let newData = await fetch(`https://www.omdbapi.com/?apikey=13103cab&i=${item.imdbID}`)
+            let movie = await newData.json()
                     moviesEl.innerHTML += `
                     <div class="movie">
-                    <img src="${movie.Poster}" class="movie-poster"> 
-                    <div class="info">
-                        <div class="title-rate">
-                            <h2>${movie.Title}</h2>
-                            <span class="rate">
-                            <i class="bi bi-star-fill"></i>
-                            ${movie.imdbRating}
-                            </span>
-                        </div>
-                        <div class="sub-info">
+                        <img src="${movie.Poster}" class="movie-poster"> 
+                        <div class="info">
+                            <div class="title-rate">
+                                <h2>${movie.Title}</h2>
+                                <span class="rate">
+                                <i class="bi bi-star-fill"></i>
+                                ${movie.imdbRating}
+                                </span>
+                            </div>
+                            <div class="sub-info">
                             <p>${movie.Runtime}</p>
                             <p>${movie.Genre}</p>
-                            <button class="watchlist-btn" data-movie=${JSON.stringify(movie.imdbID)}>
-                                <i class="bi bi-plus-circle-fill"></i>
-                                Watchlist
+                                <button class="btn watchlist-btn" data-movie=${JSON.stringify(movie.imdbID)}>
+                                    <i class="bi bi-plus-circle-fill"></i>
+                                    Watchlist
                                 </button>
-                                </div>
-                                <p class="story">${movie.Plot}</p>
-                                </div>
-                                </div>
-                                `
+                            </div>
+                            <p class="story">${movie.Plot}</p>
+                        </div>
+                    </div>
+                    `
                 })
-        });
     } catch (err) {
         console.log(err)
     }
@@ -63,8 +61,14 @@ searchForm.addEventListener("submit", (e) => {
 document.addEventListener("click", async (e) => {
     if (e.target.classList.contains("watchlist-btn")) {
         const imdbID = e.target.dataset.movie;
+        e.target.disabled = true;
 
         if (watchlist.some(movie => movie.imdbID === imdbID)) {
+            Swal.fire({
+                title: "Ohoo ",
+                text: "Movie already exists in watchlist!",
+                icon: "error"
+            });
             return;
         }
 
@@ -76,6 +80,11 @@ document.addEventListener("click", async (e) => {
             const movieData = await res.json();
             watchlist.push(movieData);
             localStorage.setItem('watchlist', JSON.stringify(watchlist));
+            Swal.fire({
+                title: "Success",
+                text: "Movie added to watchlist!",
+                icon: "success"
+              });
         } catch (error) {
             console.error('Error fetching movie details:', error);
         }
@@ -84,8 +93,26 @@ document.addEventListener("click", async (e) => {
 
 
 modeEl.addEventListener("click", () => {
-    document.body.classList.toggle("light")
-    document.getElementById("container").classList.toggle("light");
+    if(!localStorage.getItem("mode")){
+        localStorage.setItem("mode", "light")
+        console.log("added")
+    } else {
+        localStorage.setItem("mode", "");
+        console.log("deleted")
+    }
+    document.body.className = (localStorage.getItem("mode"));
     document.querySelector("span.light").classList.toggle("active");
     document.querySelector("span.dark").classList.toggle("active");
 })
+document.body.className = (localStorage.getItem("mode"));
+
+window.addEventListener('scroll', function() {
+    var stickyElement = document.getElementById('search-form');
+    var rect = stickyElement.getBoundingClientRect();
+
+    if (rect.top <= 0 && rect.bottom > 0) {
+      stickyElement.style.backgroundColor = '#008DDA'; // Change background color when in sticky mode
+    } else {
+      stickyElement.style.backgroundColor = 'transparent'; // Reset background color when not in sticky mode
+    }
+  });
--- watchlist.js
+++ watchlist.js
@@ -2,34 +2,67 @@ const watchEl = document.getElementById("watchlist");
 const modeEl = document.getElementById("mode");
 let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
 
-
-watchlist.forEach(movie => {
-    watchEl.innerHTML += `
+function renderWatchlist(movies) {
+    if(movies.lenght) {
+        watchEl.innerHTML = "";
+        movies.forEach(movie => {
+            watchEl.innerHTML += `
     <div class="movie">
         <img src="${movie.Poster}" class=movie-poster> 
         <div class="info">
-            <div class="title-rate">
-                <h2>${movie.Title}</h2>
-                <span class="rate">
-                    <i class="bi bi-star-fill"></i>
-                    ${movie.imdbRating}
+        <div class="title-rate">
+        <h2>${movie.Title}</h2>
+        <span class="rate">
+        <i class="bi bi-star-fill"></i>
+        ${movie.imdbRating}
                 </span>
-            </div>
-            <div class="sub-info">
+                </div>
+                <div class="sub-info">
                 <p>${movie.Runtime}</p>
                 <p>${movie.Genre}</p>
-            </div>
+                <button class="btn remove-btn" data-movie=${JSON.stringify(movie.imdbID)}>
+                <i class="bi bi-trash3-fill"></i>
+                Remove
+                </button>
+                </div>
             <p class="story">${movie.Plot.slice(0, 230)}</p>
-        </div>
-    </div>
-    `
+            </div>
+            </div>
+            `
+        });
+    }
+}
+renderWatchlist(watchlist)
+
+document.addEventListener("click", async (e) => {
+    if (e.target.classList.contains("remove-btn")) {
+        const imdbID = e.target.dataset.movie;
+        
+        watchlist = watchlist.filter(movie => {
+            return movie.imdbID != imdbID;
+        })
+        
+        localStorage.setItem('watchlist', JSON.stringify(watchlist));
+        Swal.fire({
+            title: "Success",
+            text: "Movie removed from watchlist!",
+            icon: "success"
+        });
+        renderWatchlist(watchlist)
+    }
 });
 
 
-
 modeEl.addEventListener("click", () => {
-    document.body.classList.toggle("light")
-    document.getElementById("container").classList.toggle("light");
+    if(!localStorage.getItem("mode")){
+        localStorage.setItem("mode", "light")
+        console.log("added")
+    } else {
+        localStorage.setItem("mode", "");
+        console.log("deleted")
+    }
+    document.body.className = (localStorage.getItem("mode"));
     document.querySelector("span.light").classList.toggle("active");
     document.querySelector("span.dark").classList.toggle("active");
 })
+document.body.className = (localStorage.getItem("mode"));
